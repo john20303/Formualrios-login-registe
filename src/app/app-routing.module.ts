3b3a6fd55fe7ce5d0127965dce34afd5e7fa39dc
John Garzon <jhgarzonmedina@hotmail.com>
2021-08-12 15:53:37 -0500

    Se crean el archivo de routing,template y reactive

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveComponent } from './components/pages/reactive/reactive.component';
+import { TemplateComponent } from './components/pages/template/template.component';
+
+const routes: Routes = [
+  { path: 'template', component: TemplateComponent },
+  { path: 'reactive', component: ReactiveComponent },
+  { path: '**', pathMatch:'full', redirectTo: 'template' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/pages/template/template.component';
+import { ReactiveComponent } from './components/pages/reactive/reactive.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
