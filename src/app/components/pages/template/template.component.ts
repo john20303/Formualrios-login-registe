3059746bde2416a51112b6dc539db385792c87b8
John Garzon <jhgarzonmedina@hotmail.com>
2021-08-13 06:54:50 -0500

    creamos el formulario template con su funcionalidad y validaciónes

--- src/app/components/pages/template/template.component.ts
+++ src/app/components/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -8,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario = {
+    nombre : "",
+    apellido : "",
+    correo : ""
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  formData(form: NgForm){
+
+    if(form.invalid){
+
+      Object.values(form.controls).forEach(control =>{
+        control.markAllAsTouched();
+      })
+      return;
+    }
+    console.log(form.value);
+    form.resetForm();
   }
 
 }
